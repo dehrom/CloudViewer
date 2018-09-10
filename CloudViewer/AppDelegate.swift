413416eae61b018bb87fd9c263f14358c5687d71
Валерий Коканов <vkokanov@alfabank.ru>
2018-09-11 01:20:01 +0300

    Initial Commit

---
+++ CloudViewer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CloudViewer
+//
+//  Created by Валерий Коканов on 11.09.2018.
+//  Copyright © 2018 my-night. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
